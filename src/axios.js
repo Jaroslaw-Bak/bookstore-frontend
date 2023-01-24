2f9f6556cc0643018dd1502ac6794b7d127c4e5b
Jarosław Bąk <jarodelphi@gmail.com>
2023-01-24 23:40:21 +0100

    netifly fix2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://178.43.128.110/api/v1/',
+	baseURL: 'https://178.43.128.110/api/v1/',
 });
 
 export default instance;
